a59ea3d15fd14a44e29b8c41f1cfdb7806f41202
nguyentrson123@outlook.com <nguyentrson123@outlook.com>
2020-04-16 23:21:25 +0700

    Add project files.

---
+++ QLsinhvien/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLsinhvien
+{
+	class Program
+	{
+		public static List<sinhvien> list = new List<sinhvien>();
+		static void Main(string[] args)
+		{
+			menu();
+			Console.ReadKey();
+		}
+		public static void menu()
+		{
+			Console.WriteLine("1. Nhap thong tin : ");
+			Console.WriteLine("2. Xuat thong tin : ");
+			Console.WriteLine("3.Sua thong tin : ");
+			Console.WriteLine("4. Xoa thong tin: ");
+			Console.WriteLine("5. Tim kiem theo MSSV : ");
+			Console.WriteLine("6. Cap nhat ");
+			Console.WriteLine("7. Thoat : ");
+			Console.WriteLine("Ban lua chon : ");
+			int lc = int.Parse(Console.ReadLine());
+			switch (lc)
+			{
+				case 1: nhapds(); menu(); break;
+				case 2: xuatds(); menu(); break;
+				case 3: sua(); menu(); break;
+				case 4: xoa(); menu();break;
+				case 5: timkiem();menu();break;
+				case 6: Console.WriteLine("Da cap nhat");menu(); break;
+				case 7: break;
+			}
+		}
+		public static void nhapds()
+		{
+			Console.WriteLine("Nhap so sinh vien can them : ");
+			int sosv = int.Parse(Console.ReadLine());
+			for(int i=0; i<sosv; i++)
+			{
+				sinhvien sv = new sinhvien();
+				sv.nhap();
+				list.Add(sv);
+			}
+		}
+		public static void xuatds()
+		{
+			for( int i=0; i<list.Count; i++)
+			{
+				list[i].xuat();
+			}
+		}
+		public static void timkiem()
+		{
+			Console.WriteLine("Nhap mssv can tim kiem : ");
+			string tk= Console.ReadLine();
+			for(int i=0;i<list.Count;i++)
+			{
+				if(list[i].Mssv== tk)
+				{
+					list[i].xuat();
+				}
+			}
+		}
+		public static void xoa()
+		{
+			Console.WriteLine("Nhap mssv can xoa : ");
+			string ms = Console.ReadLine();
+			for (int i = 0; i < list.Count; i++)
+			{
+				if (list[i].Mssv == ms)
+				{
+					list.RemoveAt(i);
+				}
+			}
+		}
+		public static void sua()
+		{
+			Console.WriteLine("Nhap mssv can sua : ");
+			string ms = Console.ReadLine();
+			for (int i = 0; i < list.Count; i++)
+			{
+				if (list[i].Mssv == ms)
+				{
+					list[i].nhap();
+				}
+			}
+		}
+	}
+}
---
+++ QLsinhvien/sinhvien.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLsinhvien
+{
+	class sinhvien
+	{
+		private string mssv;
+		private string hoten;
+		private string khoa;
+		private int diemht;
+		private int diemrl;
+		private string hang;
+		public string Mssv
+		{
+			get
+			{
+				return mssv;
+			}
+			set
+			{
+				mssv = value;
+			}
+		}
+		
+		public string Hoten
+		{
+			get
+			{
+				return hoten;
+			}
+			set
+			{
+				hoten = value;
+			}
+		}
+		public string Khoa
+		{
+			get
+			{
+				return khoa;
+			}
+			set
+			{
+				khoa = value;
+			}
+		}
+		public int Diemht
+		{
+			get
+			{
+				return diemht;
+			}
+			set
+			{
+				diemht = value;
+			}
+		}
+		public int Diemrl
+		{
+			get
+			{
+				return diemrl;
+			}
+			set
+			{
+				diemrl = value;
+			}
+		}
+		public string Hang
+		{
+			get
+			{
+				return hang;
+			}
+			set
+			{
+				hang = value;
+			}
+		}
+
+		public sinhvien() { }
+		public sinhvien(string mssv, string hoten, string khoa, int diemht, int diemrl)
+		{
+			this.Mssv = mssv;
+			this.Hoten = hoten;
+			this.Khoa = khoa;
+			this.Diemht = diemht;
+			this.Diemrl = diemrl;
+
+		}
+		public void nhap()
+		{
+			Console.Write("Nhap ma so sinh vien : ");
+			Mssv = Console.ReadLine();
+			Console.Write("Nhap  ho ten sinh vien : ");
+			Hoten = Console.ReadLine();
+			Console.Write("Nhap khoa sinh vien : ");
+			Khoa = Console.ReadLine();
+			Console.Write("Nhap diem hoc tap : ");
+			Diemht = int.Parse(Console.ReadLine());
+			Console.Write("Nhap diem ren luyen : ");
+			Diemrl = int.Parse(Console.ReadLine());
+			if(Diemht>= 9 && Diemrl >=90 ) { hang = "Xuat xac "; }
+			else if (Diemht >= 9 && Diemrl >= 80 && Diemrl<90 ) { hang = "Gioi "; }
+			else if (Diemht >= 9 && Diemrl >= 70 && Diemrl < 80) { hang = "Kha "; }
+			else if (Diemht >= 8 && Diemrl >= 80 ) { hang = "Gioi "; }
+			else if (Diemht >= 7 && Diemrl >= 70 ) { hang = "Kha "; }
+
+			else { hang = "YEU"; }
+
+
+
+
+		}
+		public void xuat()
+		{
+			string chuoi = string.Format("{0,15}{1,15}{2,15}{3,15}{4,15}{5,15}", this.Mssv, this.Hoten, this.Khoa, this.Diemht, this.Diemrl, this.Hang);
+			Console.WriteLine(chuoi);	
+		}
+
+
+
+	}
+
+
+}
